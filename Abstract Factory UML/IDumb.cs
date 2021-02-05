fd160bbae0fa257a7dad09668e53033502d4c0e1
Mason <Mason@DESKTOP-NHDIDLB>
2021-02-05 03:43:33 -0600

    Add project files.

---
+++ Abstract Factory UML/IDumb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory_UML
+{
+    interface IDumb
+    {
+        string getName();
+    }
+        class Asha : IDumb
+        {
+            public string getName()
+            {
+                return "Asha";
+            }
+        }
+        class Genie : IDumb
+        {
+            public string getName()
+            {
+                return "Genie";
+            }
+        }
+        class Primo : IDumb
+        {
+            public string getName()
+            {
+                return "Primo";
+            }
+        }
+}
